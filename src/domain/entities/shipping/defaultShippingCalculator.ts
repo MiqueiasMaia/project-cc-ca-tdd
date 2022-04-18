d574e897881ce131ee33f7bf68d0ecc0efe393b4
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-04-18 19:27:54 -0300

    feat: improve default shipping calculator

--- src/domain/entities/shipping/defaultShippingCalculator.ts
+++ src/domain/entities/shipping/defaultShippingCalculator.ts
@@ -3,9 +3,23 @@ import Shipping from './shipping';
 
 export default class DefaultShippingCalculator implements Shipping {
     private DISTANCE = 1000;
+    private total: number;
+
+    constructor() {
+        this.total = 0;
+    }
+
+    addItem(item: Item, quantity: number): void {
+        this.total += this.calculate(item) * quantity;
+    }
+
     calculate(item: Item): number {
         const minShipping = 10;
-        const shipping = (this.DISTANCE * item.getVolume() * (item.getDensity()/100));
+        const shipping = (this.DISTANCE * item.getVolume() * (item.getDensity() / 100));
         return shipping > minShipping ? shipping : minShipping;
+    }
+
+    getTotal(): number {
+        return this.total;
     }
 }
